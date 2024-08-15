6d19c087ffe6a20293aa707a778838d0b5ccbc59
Giordano26 <smfgiordano@gmail.com>
2024-08-15 17:25:08 -0300

    feat: new questions

--- src/pages/Dicas/index.js
+++ src/pages/Dicas/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -12,6 +12,29 @@ import "./Dicas.css"
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 
+const perguntas = [ 
+  {
+    pergunta: "O que é reciclagem e por que é importante?",
+    resposta: "Reciclagem é o processo de transformar materiais usados em novos produtos. É importante porque reduz a quantidade de lixo nos aterros, economiza recursos naturais e diminui a poluição."
+  },
+  {
+    pergunta: "Como posso economizar água diariamente?",
+    resposta: "Feche a torneira enquanto escova os dentes, conserte vazamentos, tome banhos mais curtos e utilize um balde para lavar o carro em vez de mangueira."
+  },
+  {
+    pergunta: "Qual é o impacto do uso excessivo de plástico no meio ambiente?",
+    resposta: "O plástico leva centenas de anos para se decompor, polui oceanos, prejudica a vida marinha e libera toxinas no ambiente, contribuindo para a degradação ambiental."
+  },
+  {
+    pergunta: "O que posso fazer para reduzir a poluição do ar?",
+    resposta: "Use transporte público, ande de bicicleta, opte por veículos elétricos ou híbridos, evite queimar lixo e plante árvores para ajudar na absorção de dióxido de carbono."
+  },
+  {
+    pergunta: "Quais são as vantagens de usar energia renovável?",
+    resposta: "Energia renovável, como solar e eólica, é limpa, inesgotável e reduz as emissões de gases de efeito estufa, ajudando a combater as mudanças climáticas."
+  }
+
+]
 
 function Dicas() {
   return (
@@ -24,9 +47,11 @@ function Dicas() {
           modules={[EffectCards]}
           className="mySwiper"
           >
-          <SwiperSlide><Card frontSide="teste" backSide="vire" title="titulo"/></SwiperSlide>
-          <SwiperSlide><Card frontSide="teste2" backSide="vire2" title="titulo2"/></SwiperSlide>
-          <SwiperSlide><Card frontSide="teste3" backSide="vire3" title="titulo3"/></SwiperSlide>
+          <SwiperSlide><Card frontSide={perguntas[0].pergunta} backSide={perguntas[0].resposta} title="Pergunta 1"/></SwiperSlide>
+          <SwiperSlide><Card frontSide={perguntas[1].pergunta} backSide={perguntas[1].resposta} title="Pergunta 2"/></SwiperSlide>
+          <SwiperSlide><Card frontSide={perguntas[2].pergunta} backSide={perguntas[2].resposta} title="Pergunta 3"/></SwiperSlide>
+          <SwiperSlide><Card frontSide={perguntas[3].pergunta} backSide={perguntas[3].resposta} title="Pergunta 4"/></SwiperSlide>
+          <SwiperSlide><Card frontSide={perguntas[4].pergunta} backSide={perguntas[4].resposta} title="Pergunta 5"/></SwiperSlide>
           </Swiper>
           
         </div>
