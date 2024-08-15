a38d696e1893f188367d560b98e32f6b011fd05a
Stefano Giordano <Stef@Campanario.local>
2024-08-14 22:46:27 -0300

    project setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,3 @@
+import React from "react"
+import {link} from 'react-router-dom'
+import logo from "../../assets/imgs/logo.png"
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./styles.module.css"
+
+
+function Home () {
+return(
+    <div>
+
+        <h1>Ola mundo</h1>
+
+    </div>
+)}
+
+export default Home
